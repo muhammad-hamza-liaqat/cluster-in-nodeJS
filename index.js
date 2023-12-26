c1bdd019936a3387d9574defd04bfe5e9ca243cd
Muhammad Hamza <mh408800@gmail.com>
2023-12-26 18:56:26 +0500

    number of cpus

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cluster = require('node:cluster');
 const numCPUs = require('node:os').availableParallelism();
-
+const os = require("os");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+
+const totalCPUs = os.cpus().length;
+console.log(totalCPUs)
 
 
 
