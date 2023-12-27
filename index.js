28f6631c0fff2cc5ea94d1ce6f4fd4ff09e51b89
Muhammad Hamza <mh408800@gmail.com>
2023-12-27 10:29:47 +0500

    .

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const cluster = require('node:cluster');
-const numCPUs = require('node:os').availableParallelism();
+const cluster = require("node:cluster");
+const numCPUs = require("node:os").availableParallelism();
 const os = require("os");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 const totalCPUs = os.cpus().length;
-console.log(totalCPUs)
-console.log(cluster.isPrimary)
+console.log(totalCPUs);
 
+if (cluster.isPrimary) {
+  console.log(`primary ${process.id} is running`);
+}
 
-
-app.listen(PORT, ()=>{
-    console.log(`server is running at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
